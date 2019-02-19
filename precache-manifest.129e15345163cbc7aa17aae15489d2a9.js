1a3a52738058e45e3f88dbcc46df22cadd1f4f23
Dhruva Nilakantan <Dhruva@NayantaraMacBook.local>
2019-02-18 18:54:13 -0800

    Updates

---
+++ precache-manifest.129e15345163cbc7aa17aae15489d2a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5c38454ffc73f453f41",
+    "url": "/speedometer/static/css/main.b150a827.chunk.css"
+  },
+  {
+    "revision": "d5c38454ffc73f453f41",
+    "url": "/speedometer/static/js/main.d5c38454.chunk.js"
+  },
+  {
+    "revision": "6b3b27f2c115e6a58569",
+    "url": "/speedometer/static/js/runtime~main.6b3b27f2.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/speedometer/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/speedometer/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "49cd6bdcfcfd8e257553205eeb9f7b76",
+    "url": "/speedometer/index.html"
+  }
+];
